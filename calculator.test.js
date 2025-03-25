e6ff5e70cd7ccb002502f18d59e9c469a00de63a
Dibyakanta Barik <dibyakanta.barik@daffodilsw.com>
2025-03-25 20:58:57 +0530

    Add support for single number.

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require("./calculator");
 test("empty string returns 0", () => {
   expect(calculator.add("")).toBe(0);
 });
+
+test('single number returns the number itself', () => {
+    expect(calculator.add("1")).toBe(1);
+  });
