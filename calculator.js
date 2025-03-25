48f45b0be1b8d22d6a480797d0f570b6c43719f8
Dibyakanta Barik <dibyakanta.barik@daffodilsw.com>
2025-03-25 21:04:45 +0530

    Add support for new line delimiters.

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ module.exports = {
     if (numbers === "") {
       return 0;
     }
-    const numArray = numbers.split(",");
-    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+    const numArray = numbers.split(/,|\n/);
+    return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
   },
 };
--- calculator.test.js
+++ calculator.test.js
@@ -12,6 +12,10 @@ test("two numbers return their sum", () => {
   expect(calculator.add("1,5")).toBe(6);
 });
 
-test('multiple numbers return their sum', () => {
-    expect(calculator.add("1,2,3")).toBe(6);
-  });
+test("multiple numbers return their sum", () => {
+  expect(calculator.add("1,2,3")).toBe(6);
+});
+
+test("handles new lines between numbers", () => {
+  expect(calculator.add("1\n2,3")).toBe(6);
+});
